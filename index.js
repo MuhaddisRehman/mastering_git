b5bbf13c42cc1f7e09df8b8a6f0407eec7af554d
Muhaddis <muhaddisrehman12@gmail.com>
2025-04-30 11:44:14 +0500

    hello and test files

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-import express from "express";
-
-const app = express();
-const PORT = 3000;
-app.get("/", (req, res) => {
-  res.status(200).json({
-    hello: "Hello World",
-  });
-});
-app.listen(PORT, () => {
-  console.log("listening on port " + PORT);
-});
+console.log("Hello World");
